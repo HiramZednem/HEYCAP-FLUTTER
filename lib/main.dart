5ee3813a9811f963a79e813a8c79b8cae8ceb81a
HiramZednem <hirammendez000@gmail.com>
2024-09-09 12:40:53 -0600

    REFACTOR: rename files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:heycap/screen/contacts.dart';
-import 'package:heycap/screen/info.dart';
+import 'package:heycap/screen/home.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
