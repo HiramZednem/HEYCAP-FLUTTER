3263618cf22c0e7fa083f3f4eec2258ff5280b39
HiramZednem <hirammendez000@gmail.com>
2024-09-17 15:43:17 -0600

    FIX: problems with the alining in contact_info

--- lib/screen/contact_info.dart
+++ lib/screen/contact_info.dart
@@ -19,57 +19,60 @@ class ContactInfo extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Información de Contacto'),
+        backgroundColor: Colors.orange,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Imagen de perfil
-            CircleAvatar(
-              radius: 80,
-              backgroundImage: NetworkImage(
-                'https://scontent.fntr7-1.fna.fbcdn.net/v/t39.30808-6/449778905_506681621686778_7264212319723498870_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHeGU8benS3mlutpYVo0st1_6zZHa_mFOL_rNkdr-YU4lMFpP6cTGmSNDNF4nidYBPKBZAvYtQfoFxM8etANR15&_nc_ohc=-eQHpgyu6_IQ7kNvgFtiOFT&_nc_zt=23&_nc_ht=scontent.fntr7-1.fna&oh=00_AYAWbkhPBBJVuyz_5aTngS_EKK0Efcp6ku1dcZewu2GYPw&oe=66EFAB62',
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Imagen de perfil
+              const CircleAvatar(
+                radius: 80,
+                backgroundImage: NetworkImage(
+                  'https://scontent.fntr7-1.fna.fbcdn.net/v/t39.30808-6/449778905_506681621686778_7264212319723498870_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHeGU8benS3mlutpYVo0st1_6zZHa_mFOL_rNkdr-YU4lMFpP6cTGmSNDNF4nidYBPKBZAvYtQfoFxM8etANR15&_nc_ohc=-eQHpgyu6_IQ7kNvgFtiOFT&_nc_zt=23&_nc_ht=scontent.fntr7-1.fna&oh=00_AYAWbkhPBBJVuyz_5aTngS_EKK0Efcp6ku1dcZewu2GYPw&oe=66EFAB62',
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
+              const SizedBox(height: 16),
 
-            // Información personal
-            const Text(
-              'Nombre: Hiram Mendez',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              'Matrícula: 213456',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              'Grupo: 9b',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 16),
+              // Información personal
+              const Text(
+                'Nombre: Hiram Mendez',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'Matrícula: 213456',
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'Grupo: 9b',
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 16),
 
-            // Enlaces a GitHub y LinkedIn
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.link),
-                  onPressed: () => _launchURL('https://github.com/HiramZednem'),
-                  tooltip: 'GitHub',
-                ),
-                const SizedBox(width: 16),
-                IconButton(
-                  icon: const Icon(Icons.business),
-                  onPressed: () =>
-                      _launchURL('https://www.linkedin.com/in/hiramzednem/'),
-                  tooltip: 'LinkedIn',
-                ),
-              ],
-            ),
-          ],
+              // Enlaces a GitHub y LinkedIn
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.link),
+                    onPressed: () =>
+                        _launchURL('https://github.com/HiramZednem'),
+                    tooltip: 'GitHub',
+                  ),
+                  const SizedBox(width: 16),
+                  IconButton(
+                    icon: const Icon(Icons.business),
+                    onPressed: () =>
+                        _launchURL('https://www.linkedin.com/in/hiramzednem/'),
+                    tooltip: 'LinkedIn',
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screen/contacts.dart
+++ lib/screen/contacts.dart
@@ -23,62 +23,65 @@ class ContactsScreen extends StatelessWidget {
 
     final List<Widget> teamMembersList = teamMembers.entries.map((entry) {
       return ListTile(
-          subtitle: Text(entry.value['name'] ?? '',
-              style:
-                  const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.orange),
-                  borderRadius: BorderRadius.circular(50),
-                  color: Colors.orange.withOpacity(0.1),
-                ),
-                child: IconButton(
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                  icon: const Icon(Icons.phone),
-                  onPressed: () async {
-                    final phoneNumber = Uri.parse('tel:${entry.value['phone']}');
-                    if (await canLaunchUrl(phoneNumber)) {
-                      await launchUrl(phoneNumber);
-                    } else {
-                      throw 'Could not launch $phoneNumber';
-                    }
-                  },
-                ),
+        subtitle: Text(
+          entry.value['name'] ?? '',
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.orange),
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.orange.withOpacity(0.1),
               ),
-
-              const SizedBox(width: 10),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.orange),
-                  borderRadius: BorderRadius.circular(50),
-                  color: Colors.orange.withOpacity(0.1),
-                ),
-
-                child: IconButton(
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                  icon: const Icon(Icons.message),
-                  onPressed: () async {
-                    final menssageNumber = Uri.parse('sms:${entry.value['phone']}');
-                    if (await launchUrl(menssageNumber)) {
-                      await launchUrl(menssageNumber);
-                    } else {
-                      throw 'Could not launch $menssageNumber';
-                    }
-                  },
-                ),
-
-
-              )
-            ],
-          ));
+              child: IconButton(
+                color: const Color.fromARGB(255, 0, 0, 0),
+                icon: const Icon(Icons.phone),
+                onPressed: () async {
+                  final phoneNumber = Uri.parse('tel:${entry.value['phone']}');
+                  if (await canLaunchUrl(phoneNumber)) {
+                    await launchUrl(phoneNumber);
+                  } else {
+                    throw 'Could not launch $phoneNumber';
+                  }
+                },
+              ),
+            ),
+            const SizedBox(width: 10),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.orange),
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.orange.withOpacity(0.1),
+              ),
+              child: IconButton(
+                color: const Color.fromARGB(255, 0, 0, 0),
+                icon: const Icon(Icons.message),
+                onPressed: () async {
+                  final messageNumber =
+                      Uri.parse('sms:${entry.value['phone']}');
+                  if (await launchUrl(messageNumber)) {
+                    await launchUrl(messageNumber);
+                  } else {
+                    throw 'Could not launch $messageNumber';
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      );
     }).toList();
 
     return Scaffold(
-        body: Center(
-      child: Column(
+      appBar: AppBar(
+        title: const Text('Contactos'),
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -89,8 +92,9 @@ class ContactsScreen extends StatelessWidget {
             Column(
               children: teamMembersList,
             ),
-
-          ]),
-    ));
+          ],
+        ),
+      ),
+    );
   }
 }
